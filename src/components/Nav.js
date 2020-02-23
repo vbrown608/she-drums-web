dd3a7bc1892f04c2b28dc8b9c17378fd327f5d15
Vivian Brown <vivian@netlify.com>
2020-02-22 19:06:07 -0800

    Cleaning up main nav branding

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -45,11 +45,8 @@ export class Navigation extends Component {
     return (
       <nav className={`Nav ${active ? 'Nav-active' : ''}`}>
         <div className="Nav--Container container">
-          <Link to="/" onClick={this.handleLinkClick}>
-            <Logo />
-          </Link>
           <div className="Nav--Links">
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/">SHE DRUMS PROJECT</NavLink>
             <NavLink to="/about/">About</NavLink>
             <NavLink to="/interviews/">Interviews</NavLink>
             <NavLink to="/resources/">Resources</NavLink>
