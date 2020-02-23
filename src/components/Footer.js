fa5b901f8792f8f577e6a89ddac6c0f15be81bb0
Vivian Brown <vivian@netlify.com>
2020-02-23 08:59:20 -0800

    Add footer form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,19 @@
 import React from 'react'
 import InstagramFeed from './InstagramFeed'
+import FormFooter from './FormFooter'
 import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
-    <br />
-    <InstagramFeed count="8" />
     <footer className="footer">
-      <div className="container taCenter">
-        <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
-        </span>
+      <div className="container">
+        <div className="split">
+          <FormFooter />
+          <span>
+            © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
+            <a href="https://thriveweb.com.au/">Thrive</a>.
+          </span>
+        </div>
       </div>
     </footer>
   </div>
---
+++ src/components/FormFooter.js
@@ -0,0 +1,111 @@
+
+import React, { Fragment } from 'react'
+import Helmet from 'react-helmet'
+import { stringify } from 'qs'
+import { serialize } from 'dom-form-serializer'
+
+import './Form.css'
+
+class FormFooter extends React.Component {
+  static defaultProps = {
+    name: 'Simple Form Ajax',
+    subject: '', // optional subject of the notification email
+    action: '',
+    successMessage: 'Thank you for signing up for updates to the She Drums Project!',
+    errorMessage:
+      'There is a problem, your message has not been sent, please try contacting us via email'
+  }
+
+  state = {
+    alert: '',
+    disabled: false
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.disabled) return
+
+    const form = e.target
+    const data = serialize(form)
+    this.setState({ disabled: true })
+    fetch(form.action + '?' + stringify(data), {
+      method: 'POST'
+    })
+      .then(res => {
+        if (res.ok) {
+          return res
+        } else {
+          throw new Error('Network error')
+        }
+      })
+      .then(() => {
+        form.reset()
+        this.setState({
+          alert: this.props.successMessage,
+          disabled: false
+        })
+      })
+      .catch(err => {
+        console.error(err)
+        this.setState({
+          disabled: false,
+          alert: this.props.errorMessage
+        })
+      })
+  }
+
+  render() {
+    const { name, subject, action } = this.props
+
+    return (
+      <Fragment>
+        <Helmet>
+          <script src="https://www.google.com/recaptcha/api.js" />
+        </Helmet>
+        <form
+          className="Form"
+          name={name}
+          action={action}
+          onSubmit={this.handleSubmit}
+          data-netlify=""
+        >
+          <label className="Form--Label">
+            <input
+              className="Form--Input Form--InputText"
+              type="text"
+              placeholder="Name"
+              name="name"
+              required
+            />
+            <span>Name</span>
+          </label>
+          <label className="Form--Label">
+            <input
+              className="Form--Input Form--InputText"
+              type="email"
+              placeholder="Email"
+              name="emailAddress"
+              required
+            />
+            <span>Email address</span>
+          </label>
+          {!!subject && <input type="hidden" name="subject" value={subject} />}
+          <input type="hidden" name="form-name" value={name} />
+          <div className="Form--Group">
+            <input
+              className="Button Form--SubmitButton"
+              type="submit"
+              value="Subscribe To Updates"
+              disabled={this.state.disabled}
+            />
+            {this.state.alert && (
+              <div className="Form--Alert">{this.state.alert}</div>
+            )}
+          </div>
+        </form>
+      </Fragment>
+    )
+  }
+}
+
+export default FormFooter
